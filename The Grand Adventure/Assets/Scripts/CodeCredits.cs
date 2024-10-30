cb8162cb60cfffa229ad4cefd15b9f42ce9b42b7
odieman32 <dettermanbrodie@gmail.com>
2024-10-29 20:56:04 -0400

    Level 1, Character, Colliders

---
+++ The Grand Adventure/Assets/Scripts/CodeCredits.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeCredits : MonoBehaviour
+{
+    //Code Credits:https://www.youtube.com/watch?v=RYw8lAI-_Y0&list=PLGmYIROty-5YPWeXeVnQvR9KbMIrL5cM-&index=3 by Daniel Wood
+}
